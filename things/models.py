e43f8472a0a39c3cc08242572176ad0a0c4ce4c2
istiyakc <istiyak.choudhury@kcl.ac.uk>
2022-11-28 23:11:51 +0000

    add colon to Thing class

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
-class Thing(models.Model)
+class Thing(models.Model):
     name = models.CharField(max_length = 30, unique = True)
     description = models.CharField(max_length = 120, blank = True)
     quantity = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(100)])
